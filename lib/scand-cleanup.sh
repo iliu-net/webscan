f6b6ec17f510d077fc38915d96ee46cbaa1a07b8
Alejandro Liu <alejandro_liu@hotmail.com>
2022-03-14 08:52:57 +0100

    fixed: small bug

--- lib/scand-cleanup.sh
+++ lib/scand-cleanup.sh
@@ -11,7 +11,7 @@ fi
 now=$(date +%s)
 
 find "$scanqueue" -maxdepth 1 -mindepth 1 -type d -print0 \
-	| xargs -0 stat -c '%Y %n' | while read ts fn
+	| xargs -r -0 stat -c '%Y %n' | while read ts fn
 do
   age=$(expr $now - $ts)
   if [ $age -gt 3600 ] ; then
